f5e636c431f179aa625e01aaa34cf0fc4608c3df
kloir-z <kosuke.otsu@gmail.com>
2023-11-04 20:42:27 +0900

    .

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -12,25 +12,51 @@ const Calendar: React.FC<CalendarProps> = ({ dateArray, wbsHeight }) => {
 
   return (
     <div style={{display: 'flex', flexDirection: 'column'}}>
-      <Row style={{ position: 'relative', borderBottom: 'none' }}>
-        {dateArray.map((date, index) => {
-          if (date.getDate() === 1) {
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+        <Row style={{ position: 'relative', borderBottom: 'none' }}>
+          {dateArray.map((date, index) => {
+            if (date.getDate() === 1) {
+              const left = 21 * index;
+              return (
+                <Cell 
+                  key={index} 
+                  style={{
+                    position: 'absolute',
+                    left: `${left}px`
+                  }}
+                >
+                  {date.getFullYear()}/{String(date.getMonth() + 1).padStart(2, '0')}
+                </Cell>
+              );
+            }
+            return null;
+          })}
+        </Row>
+        <Row style={{ position: 'relative' }}>
+          {dateArray.map((date, index) => {
+            let type = 'weekday';
+            if (date.getDay() === 6) type = 'saturday';
+            if (date.getDay() === 0 || isHoliday(date)) type = 'sundayOrHoliday';
             const left = 21 * index;
+
             return (
-              <Cell 
-                key={index} 
+              <Cell
+                key={index}
+                $type={type}
+                className="dayColumn"
                 style={{
                   position: 'absolute',
-                  left: `${left}px`
+                  left: `${left}px`,
+                  height: '23px',
+                  borderTop: '1px solid #80808047'
                 }}
               >
-                {date.getFullYear()}/{String(date.getMonth() + 1).padStart(2, '0')}
+                {date.getDate()}
               </Cell>
             );
-          }
-          return null;
-        })}
-      </Row>
+          })}
+        </Row>
+      </div>
       <Row style={{ position: 'relative' }}>
         {dateArray.map((date, index) => {
           let type = 'weekday';
@@ -46,11 +72,10 @@ const Calendar: React.FC<CalendarProps> = ({ dateArray, wbsHeight }) => {
               style={{
                 position: 'absolute',
                 left: `${left}px`,
-                height: `${wbsHeight-23}px`,
+                height: `${wbsHeight}px`,
                 borderTop: '1px solid #80808047'
               }}
             >
-              {date.getDate()}
             </Cell>
           );
         })}
